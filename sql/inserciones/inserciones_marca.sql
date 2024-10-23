311771f3fb5db32b26ef580ec878f62291d8836f
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 17:35:06 -0600

    Inserciones a la tabla marca

---
+++ sql/inserciones/inserciones_marca.sql
@@ -0,0 +1,30 @@
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Bic', 'Marca de artículos de papelería, como plumas y marcadores.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Clorox', 'Marca de productos de limpieza, especialmente cloro y desinfectantes.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Colgate', 'Marca de productos de higiene personal, especialmente pasta de dientes.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Pepsi', 'Marca de bebidas refrescantes.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Sony', 'Marca de productos electrónicos como audífonos y accesorios.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Mattel', 'Marca de juguetes populares como Barbie y Hot Wheels.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('L’Oréal', 'Marca de productos de belleza y cuidado personal.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Truper', 'Marca de herramientas y artículos de ferretería.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('Pedigree', 'Marca de alimentos y productos para mascotas.');
+
+INSERT INTO Marca (nombre_marca, descripcion_marca)
+VALUES ('3M', 'Marca de productos como cintas adhesivas y artículos para oficina.');
+
