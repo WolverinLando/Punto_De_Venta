2f69c0a1b20e0ad6205c4cd9eb4cbccca3ebb436
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 17:26:57 -0600

    Inserciones a la tabla Categoria

--- sql/inserciones/insercion_categoria.sql
+++ sql/inserciones/insercion_categoria.sql
@@ -1,1 +1,30 @@
-iii
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Papelería', 'Artículos de oficina, útiles escolares y accesorios de escritura.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Limpieza', 'Productos de limpieza para el hogar y oficinas.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Higiene personal', 'Productos de aseo personal como jabones, shampoos, y cremas.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Alimentos y bebidas', 'Comestibles y bebidas no alcohólicas.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Electrónica', 'Accesorios electrónicos como cargadores, audífonos, y cables.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Juguetería', 'Juguetes y productos recreativos para niños.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Belleza', 'Productos de cuidado personal y belleza, como maquillaje y cremas.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Ferretería', 'Herramientas básicas, artículos de ferretería y accesorios.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Mascotas', 'Alimentos, juguetes y accesorios para mascotas.');
+
+INSERT INTO Categoria (nombre_categoria, descripcion_categoria)
+VALUES ('Regalos', 'Artículos para regalos como tarjetas, envolturas y detalles.');
+
