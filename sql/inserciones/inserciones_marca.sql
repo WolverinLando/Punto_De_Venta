c471c7d782253be474f145c47e2fb7acabad1f6c
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 17:35:55 -0600

    Inserciones a la tabla marca

--- sql/inserciones/inserciones_marca.sql
+++ sql/inserciones/inserciones_marca.sql
@@ -10,21 +10,4 @@ VALUES ('Colgate', 'Marca de productos de higiene personal, especialmente pasta 
 INSERT INTO Marca (nombre_marca, descripcion_marca)
 VALUES ('Pepsi', 'Marca de bebidas refrescantes.');
 
-INSERT INTO Marca (nombre_marca, descripcion_marca)
-VALUES ('Sony', 'Marca de productos electrónicos como audífonos y accesorios.');
-
-INSERT INTO Marca (nombre_marca, descripcion_marca)
-VALUES ('Mattel', 'Marca de juguetes populares como Barbie y Hot Wheels.');
-
-INSERT INTO Marca (nombre_marca, descripcion_marca)
-VALUES ('L’Oréal', 'Marca de productos de belleza y cuidado personal.');
-
-INSERT INTO Marca (nombre_marca, descripcion_marca)
-VALUES ('Truper', 'Marca de herramientas y artículos de ferretería.');
-
-INSERT INTO Marca (nombre_marca, descripcion_marca)
-VALUES ('Pedigree', 'Marca de alimentos y productos para mascotas.');
-
-INSERT INTO Marca (nombre_marca, descripcion_marca)
-VALUES ('3M', 'Marca de productos como cintas adhesivas y artículos para oficina.');
 
