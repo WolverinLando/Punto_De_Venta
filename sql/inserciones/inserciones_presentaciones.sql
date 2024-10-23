ab0fb1dfe4c106d638ed30479a3c5d101f74fd7b
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 17:46:18 -0600

    Inserciones a la tabla presentaciones

---
+++ sql/inserciones/inserciones_presentaciones.sql
@@ -0,0 +1,30 @@
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Botella', 'Presentación en botella, generalmente utilizada para líquidos.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Caja', 'Producto empaquetado en una caja.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Bolsa', 'Producto en presentación de bolsa, ideal para alimentos o artículos pequeños.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Lata', 'Presentación en lata, común para alimentos y bebidas.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Paquete', 'Producto en conjunto, empaquetado en paquetes.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Sobre', 'Producto empaquetado en un sobre, generalmente artículos pequeños o muestras.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Blíster', 'Presentación en blíster, usualmente para pastillas o pequeños artículos electrónicos.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Frasco', 'Producto contenido en frascos, común para cremas o medicamentos.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Tubo', 'Producto en tubo, común para pastas, cremas o cosméticos.');
+
+INSERT INTO Presentacion (tipo_presentacion, descripcion_presentacion)
+VALUES ('Tarro', 'Producto en presentación de tarro, usado para alimentos como mermeladas o productos sólidos.');
+
