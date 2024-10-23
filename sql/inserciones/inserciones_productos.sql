9b2ade9ea6345f5c66511f32bc7a0b910724b85c
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 17:50:10 -0600

    Inserciones a la tabla de productos

---
+++ sql/inserciones/inserciones_productos.sql
@@ -0,0 +1,30 @@
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7501020142012', 1, 1, 1, 'Pluma Bic Azul', 'Pluma de tinta azul de la marca Bic', 5.50, 100, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7509546061234', 2, 2, 2, 'Cloro Clorox 1L', 'Cloro de un litro de la marca Clorox', 22.00, 50, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7501000000003', 3, 3, 3, 'Pasta Colgate 100g', 'Pasta dental de 100 gramos de la marca Colgate', 25.00, 30, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7500001209876', 4, 4, 4, 'Refresco Pepsi 355ml', 'Refresco en lata de 355 ml de la marca Pepsi', 12.50, 200, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7501234567890', 5, 5, 5, 'Audífonos Sony', 'Audífonos inalámbricos de la marca Sony', 499.99, 20, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7501111222233', 6, 6, 6, 'Hot Wheels', 'Auto de juguete de la marca Hot Wheels', 75.00, 150, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7509876543210', 7, 7, 7, 'Crema L’Oréal', 'Crema facial hidratante de la marca L’Oréal', 199.99, 40, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7505432109876', 8, 8, 8, 'Martillo Truper', 'Martillo de acero de la marca Truper', 120.00, 35, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7503214567890', 9, 9, 9, 'Alimento para Perro Pedigree', 'Alimento para perro de 2kg de la marca Pedigree', 89.00, 60, 'A');
+
+INSERT INTO Productos (codigo_barras, id_marca, id_categoria, id_presentacion, nombre_productos, descripcion, precio, existencias, estatus)
+VALUES ('7509871234567', 10, 10, 10, 'Cinta Adhesiva 3M', 'Cinta adhesiva de 5 metros de la marca 3M', 45.00, 80, 'A');
+
