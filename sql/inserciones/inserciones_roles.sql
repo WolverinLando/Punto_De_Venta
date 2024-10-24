bb6be7168f40fcbf16f29b602c347531546474b0
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 18:09:26 -0600

    Inserciones a la tabla de roles

---
+++ sql/inserciones/inserciones_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO Rol (nombre_rol, descripcion_rol) 
+	VALUES ('Administrador', 'Administración completa del sistema.');
+
+INSERT INTO Rol (nombre_rol, descripcion_rol) 
+	VALUES ('Cajero', 'Encargado de registrar ventas y cobrar.');
