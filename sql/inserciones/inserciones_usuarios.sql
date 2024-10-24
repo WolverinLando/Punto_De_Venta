99dafd9af483a43bfbd43f2d19f16e89bc5917c9
WolverinLando <orlandourbanotrejo@gmail.com>
2024-10-23 19:56:06 -0600

    Inserciones a la tabla de usuarios

---
+++ sql/inserciones/inserciones_usuarios.sql
@@ -0,0 +1,6 @@
+INSERT INTO Usuario (Clv_Usuario, id_rol, Nombre, Apellido_Paterno, Apellido_Materno, Correo, Usuario, Contraseña)
+VALUES ('U001', 1, 'Juan', 'Pérez', 'González', 'juan.perez@example.com', 'juanp', 'contraseña123');
+
+INSERT INTO Usuario (Clv_Usuario, id_rol, Nombre, Apellido_Paterno, Apellido_Materno, Correo, Usuario, Contraseña)
+VALUES ('U002', 2, 'María', 'López', 'Hernández', 'maria.lopez@example.com', 'marial', 'contraseña456');
+
